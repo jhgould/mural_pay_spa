d2209900bc836cf699ae8a5773bda95397936bc8
Jack Gould <Johnhilsmangould@gmail.com>
2025-05-22 12:55:34 -0700

    add spec

---
+++ spec/feature/user_creates_account_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+
+RSpec.feature 'User creates account', type: :feature do
+  scenario 'User fills in the form and submits' do
+
+    allow_any_instance_of(MuralPay::CreateAccount).to receive(:call).and_return(
+      {
+        id: 'fake-id-123',
+        name: 'Test Account',
+        status: 'INITIALIZING',
+        isApiEnabled: true
+      }
+    )
+
+    visit new_account_path
+
+    fill_in 'Name', with: 'Test Account'
+    fill_in "Description", with: 'This is a test account' 
+
+    click_button 'Create Account'
+
+    expect(page).to have_content('Account created successfully.')
+
+  end 
+
+end 
